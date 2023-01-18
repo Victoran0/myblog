234d8406ac6d16ae716346294775db3f195a075b
Victoran0 <victoranosunkvic@gmail.com>
2023-01-18 20:32:10 +0100

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
